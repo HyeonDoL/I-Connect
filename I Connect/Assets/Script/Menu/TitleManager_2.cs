8499e4a28e04c09867e9f82aae0ea5cf863dbe7e
정지상 <ashuatz@gmail.com>
2017-08-18 03:47:49 +0900

    [title]

--- I Connect/Assets/Script/Menu/TitleManager_2.cs
+++ I Connect/Assets/Script/Menu/TitleManager_2.cs
@@ -5,55 +5,92 @@ using UnityEngine;
 public class TitleManager_2 : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] Names;
-
+    private GameObject Model;
     [SerializeField]
-    private bool tester;
+    private GameObject[] Names;
+    [SerializeField]
+    private Material[] Names_Materials;
+    
     [SerializeField]
     private Transform Marker;
 
-    private Quaternion defaultRotate;
-    private Quaternion ReverseRotate;
+    private const int MaxCharacter = 9;
 
+    private Quaternion[] defaultRotate = new Quaternion[MaxCharacter];
+    private Quaternion RandomRotate;
+
+    private Vector3[] defaultPosition = new Vector3[MaxCharacter];
+    
     private void Awake()
     {
         for(int i=0;i<Names.Length;++i)
         {
             Names[i].SetActive(false);
-            Names[i].transform.position = Marker.position;
+            //Names[i].transform.position = Marker.position;
+            defaultRotate[i] = Names[i].transform.rotation;
+
+            defaultPosition[i] = Names[i].transform.position;
         }
-        defaultRotate = Quaternion.Euler(-180, 0, 0);
+
+        Model.transform.rotation = defaultRotate[0];
+
+        StartCoroutine(StartTitle());
     }
 
-    private void Update()
-    {
-        if(tester)
-        {
-            tester = false;
-            StartCoroutine(StartTitle());
-        }
-    }
     public IEnumerator StartTitle()
     {
+        yield return new WaitForSeconds(0.5f);
+
         int indexer = 0;
-        Names[indexer].SetActive(true);
-        while (indexer < Names.Length-1)
+        while (indexer < Names.Length)
         {
-            yield return new WaitForSeconds(0.5f);
-            defaultRotate = Quaternion.Euler(Random.Range(1500, 2700), Random.Range(1500, 2700), Random.Range(1500, 2700));
-            StartCoroutine(Tween.TweenTransform.Rotation(Names[indexer].transform, defaultRotate, 0.25f));
-            yield return new WaitForSeconds(0.35f);
-            defaultRotate = Quaternion.Euler(Random.Range(1500, 2700), Random.Range(1500, 2700), Random.Range(1500, 2700));
-            StartCoroutine(Tween.TweenTransform.Rotation(Names[indexer].transform, defaultRotate, 0.25f));
-            yield return new WaitForSeconds(0.24f);
-            Names[indexer].SetActive(false);
-            if (indexer + 1 >= Names.Length)    break;
-            Names[++indexer].SetActive(true);
+            yield return new WaitForSeconds(0.25f);
+            yield return StartCoroutine(Rotater(Model.transform, 0.8f, ++indexer));
+            yield return StartCoroutine(Tween.TweenTransform.Rotation(Model.transform, defaultRotate[indexer], 0.5f));
+
+            if (indexer ==8)    break;
         }
         yield return new WaitForSeconds(0.5f);
-        StartCoroutine(Tween.TweenTransform.Rotation(Names[8].transform, Quaternion.Euler(0,-135,0), 0.25f));
+        StartCoroutine(Tween.TweenTransform.Rotation(Model.transform, Quaternion.Euler(0,-135,0), 0.25f));//t
         yield return new WaitForSeconds(0.5f);
 
         yield return null;
+
+        StartCoroutine(SetObjectActives());
+    }
+    private IEnumerator Rotater(Transform target, float EndTime, int indexer)
+    {
+        float timer=0f;
+        Vector3 eularRandom = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5)) * 3f;
+        while (timer< EndTime)
+        {
+            if(timer > EndTime / 2)
+                Model.GetComponent<MeshRenderer>().material = Names_Materials[indexer];
+
+
+            target.Rotate(eularRandom);
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
+
+    private IEnumerator SetObjectActives()
+    {
+        yield return StartCoroutine(Tween.TweenTransform.LocalScale(Model.transform, Vector3.one, 0.5f));
+
+        for(int i = 0; i < MaxCharacter; i++)
+        {
+            Names[i].transform.position = Model.transform.position;
+
+            Names[i].SetActive(true);
+        }
+
+        Model.SetActive(false);
+
+        for (int i = 0; i < MaxCharacter; i++)
+        {
+            StartCoroutine(Tween.TweenTransform.Position(Names[i].transform, defaultPosition[i], 0.5f));
+        }
     }
 }
