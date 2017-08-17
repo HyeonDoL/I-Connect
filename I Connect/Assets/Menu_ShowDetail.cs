bdcf282af4f2831de7a1ce9318b113d3511afe89
정지상 <ashuatz@gmail.com>
2017-08-17 21:26:53 +0900

    메뉴제작

---
+++ I Connect/Assets/Menu_ShowDetail.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct StagePair
+{
+    public RectTransform Stage;
+    public Vector3 TargetPosition;
+}
+
+public class Menu_ShowDetail : MonoBehaviour
+{
+    [SerializeField]
+    private StagePair[] Stages;
+
+    [SerializeField]
+    private float CurrentSetWaitTime;
+    private float GoTimer;
+
+    [SerializeField]
+    private float accuracy;
+
+    [SerializeField]
+    private float TargetScale;
+    private float DefaultScale;
+    private RectTransform myTransform;
+    private void Awake()
+    {
+        myTransform = this.GetComponent<RectTransform>();
+        DefaultScale = myTransform.localScale.x;
+        for (int i = 0; i < Stages.Length; ++i)
+        {
+            Stages[i].Stage.position = myTransform.position;
+            Stages[i].Stage.gameObject.SetActive(false);
+        }
+    }
+    public void OnSelect()
+    {
+        StartCoroutine(GoTargetPosition());
+    }
+    private IEnumerator ScaleUp()
+    {
+        yield return null;
+    }
+    private IEnumerator GoTargetPosition()
+    {
+        for (int i = 0; i < Stages.Length; ++i)
+        {
+            Stages[i].Stage.gameObject.SetActive(true);
+        }
+        while (GoTimer<CurrentSetWaitTime)
+        {
+            for(int i=0;i<Stages.Length;++i)
+            {
+                Stages[i].Stage.localPosition += (Stages[i].TargetPosition - Stages[i].Stage.localPosition) * 0.1f;
+            }
+            myTransform.localScale *= TargetScale;
+            GoTimer += accuracy;
+            yield return new WaitForSeconds(accuracy);
+        }
+        for (int i = 0; i < Stages.Length; ++i)
+        {
+            Stages[i].Stage.localPosition = Stages[i].TargetPosition;
+        }
+
+        yield return null;
+    }
+}
---
+++ I Connect/Assets/Script/Menu/MenuController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    //not use
+
+    //public enum MenuButtonType
+    //{
+    //    None,
+    //    TouchToStart,
+    //    Cancle,
+    //    Start,
+    //    Exit,
+    //    Host,
+    //    Join,
+    //    Refresh
+    //}
+    //[SerializeField]
+    //private MenuButtonType myType;
+
+    //[SerializeField]
+    //private GameObject[] Menus;
+
+    //[SerializeField]
+    //private GameObject Waiting;
+
+    //private bool IsUsingItem;
+
+    //public void ClickMenu()
+    //{
+    //    switch (myType)
+    //    {
+    //        case MenuButtonType.TouchToStart: DisableMenuAndTurnOn(1); break;
+    //        case MenuButtonType.Cancle: DisableMenuAndTurnOn(0); NetworkManager.instance.OnClickCancle(); break;
+    //        case MenuButtonType.Join: DisableMenuAndTurnOn(2); break;
+    //        case MenuButtonType.Host: OnClickHost(); break;
+
+    //        case MenuButtonType.Start:
+    //            AutoFade.LoadLevel(GameManager.instance.isUsingItem ? "Local_item" : "Local", 1.5f, 1.5f, Color.black); GameManager.instance.Reset(); GameManager.instance.FadeMusic.Fade(1.5f); break;
+    //        case MenuButtonType.Exit: AutoFade.LoadLevel("Title", 1.5f, 1.5f, Color.white); GameManager.instance.Reset(); break;
+
+    //        default: break;
+    //    }
+    //}
+    //private void DisableMenuAndTurnOn(int OnIndex)
+    //{
+    //    for (int i = 0; i < Menus.Length; ++i)
+    //    {
+    //        Menus[i].SetActive(false);
+    //    }
+    //    Menus[OnIndex].SetActive(true);
+    //}
+
+    //private void OnClickHost()
+    //{
+    //    if (Network.isServer || Network.isClient)
+    //        return;
+    //    NetworkManager.instance.OnClickStart("MatchPoint : " + GameManager.instance.GameRound + "\n" + "GameLevel : " + GameManager.instance.GameDifficulty);
+    //    Waiting.SetActive(true);
+
+    //}
+    //public void SetUsingItem()
+    //{
+    //    IsUsingItem = !IsUsingItem;
+    //    Debug.Log("Itme: " + IsUsingItem);
+    //}
+
+}//not use
