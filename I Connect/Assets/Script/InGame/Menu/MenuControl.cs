2799cdf871e4c4d79fe9213c43fef7677e6878d1
HyunDo_Lee <godd7776@gmail.com>
2017-08-17 17:00:51 +0900

    Create MenuControl

---
+++ I Connect/Assets/Script/InGame/Menu/MenuControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour
+{
+    [SerializeField]
+    private float controlTime = 2f;
+
+    [SerializeField]
+    private Transform menuTrans;
+
+    [SerializeField]
+    private Transform openTrans;
+
+    [SerializeField]
+    private Transform closeTrans;
+
+    private bool isOpen;
+
+    private void Awake()
+    {
+        isOpen = false;
+    }
+
+    public void Toggle()
+    {
+        if(isOpen)
+            StartCoroutine(Tween.TweenTransform.Position(menuTrans, closeTrans.position, controlTime));
+
+        else
+            StartCoroutine(Tween.TweenTransform.Position(menuTrans, openTrans.position, controlTime));
+
+        isOpen = !isOpen;
+    }
+}
