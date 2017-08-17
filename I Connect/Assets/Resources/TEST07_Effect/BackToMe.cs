686974ab941c413c16f640004fd622a7cbb06de5
정지상 <ashuatz@gmail.com>
2017-08-17 17:11:37 +0900

    Particle Set

---
+++ I Connect/Assets/Resources/TEST07_Effect/BackToMe.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMe : MonoBehaviour
+{
+    ParticleSystem.Particle[] cloud = new ParticleSystem.Particle[1000];
+    ParticleSystem _particleSystem;
+
+    [Header("CallTime :default -> 0")]
+    [SerializeField]
+    private float CallTime;
+
+    [SerializeField]
+    private float CallMultipler;
+    [Header("BackSpeed :  default -> Call multipler / (1-callMultipler)")]
+    [SerializeField]
+    private float BackSpeed;
+
+    private bool Playing
+    {
+        set
+        {
+            if (value)
+            {
+                _particleSystem.Play();
+                StartCoroutine(EffectFunc(CallTime));
+            }
+            else
+            {
+                _particleSystem.Stop();
+                _particleSystem.Clear();
+            }
+        }
+        get
+        {
+            return _particleSystem.isPlaying;
+        }
+    }
+    public Transform myTransform;
+
+    private void Awake()
+    {
+        _particleSystem = this.GetComponent<ParticleSystem>();
+
+        if (CallTime <= 0f)
+        {
+            CallTime = CallMultipler * _particleSystem.main.startLifetime.curveMultiplier;
+        }
+    }
+    private IEnumerator EffectFunc(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        int numParticlesAlive = _particleSystem.GetParticles(cloud);
+
+        for (int i = 0; i < numParticlesAlive; ++i)
+        {
+            cloud[i].velocity *= -1f * BackSpeed;
+            _particleSystem.SetParticles(cloud, numParticlesAlive);
+        }
+        _particleSystem.SetParticles(cloud, numParticlesAlive);
+        numParticlesAlive = _particleSystem.GetParticles(cloud);
+
+        yield return null;
+    }
+    public void PlayParticle()
+    {
+        Playing = true;
+    }
+    public void StopParticle()
+    {
+        Playing = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Playing = false;
+            Playing = true;
+        }
+    }
+}
---
+++ I Connect/Assets/Resources/TEST07_Effect/TEST07_TestScaler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST07_TestScaler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Particle;
+    private BackToMe myParticleSystem;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+            ttAction();
+    }
+    void ttAction()
+    {
+        GameObject particle = GameObject.Instantiate(Particle);
+        particle.transform.parent = this.transform;
+        particle.transform.localPosition = new Vector3(0, 0, 0);
+        particle.transform.localScale = new Vector3(1, 1, 1);
+
+        myParticleSystem = particle.GetComponent<BackToMe>();
+        myParticleSystem.myTransform = this.transform;
+        myParticleSystem.PlayParticle();
+    }
+}
