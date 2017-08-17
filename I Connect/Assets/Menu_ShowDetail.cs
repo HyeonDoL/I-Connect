0d9a65999e4ebac0ccf4d4b8cd194d8b961ecb18
HyunDo_Lee <godd7776@gmail.com>
2017-08-18 03:48:17 +0900

    Fixed Line Connecting

--- I Connect/Assets/Menu_ShowDetail.cs
+++ I Connect/Assets/Menu_ShowDetail.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.EventSystems;
 
 [Serializable]
 public struct StagePair
@@ -10,7 +10,7 @@ public struct StagePair
     public Vector3 TargetPosition;
 }
 
-public class Menu_ShowDetail : MonoBehaviour
+public class Menu_ShowDetail : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
     [SerializeField]
     private StagePair[] Stages;
@@ -24,8 +24,15 @@ public class Menu_ShowDetail : MonoBehaviour
 
     [SerializeField]
     private float TargetScale;
+
     private float DefaultScale;
+
+    private bool isGoTarget;
+
     private RectTransform myTransform;
+
+    private UIMeshLine line;
+
     private void Awake()
     {
         myTransform = this.GetComponent<RectTransform>();
@@ -35,10 +42,13 @@ public class Menu_ShowDetail : MonoBehaviour
             Stages[i].Stage.position = myTransform.position;
             Stages[i].Stage.gameObject.SetActive(false);
         }
+
+        isGoTarget = false;
     }
     public void OnSelect()
     {
         StartCoroutine(GoTargetPosition());
+
     }
     private IEnumerator ScaleUp()
     {
@@ -65,6 +75,71 @@ public class Menu_ShowDetail : MonoBehaviour
             Stages[i].Stage.localPosition = Stages[i].TargetPosition;
         }
 
+        isGoTarget = true;
+
         yield return null;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!isGoTarget)
+            return;
+
+        line = ObjectPoolManager.Instance.GetObject(ObjectPoolType.Line, Vector3.zero).GetComponent<UIMeshLine>();
+
+        Vector2 objectPosition = myTransform.localPosition;
+        Vector2 position = new Vector2((objectPosition.x ) , (objectPosition.y ) );
+
+        line.SetPointPosition(0, position);
+        line.SetPointPosition(1, position);
+
+        line.lengthRatio = 1f;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!isGoTarget)
+            return;
+
+        Vector2 mousePosition = Camera.main.ScreenToViewportPoint( Input.mousePosition);
+
+        Vector2 position = new Vector2((mousePosition.x )*1600 -800f , (mousePosition.y )*900 - 450f);
+
+        line.SetPointPosition(1, position);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!isGoTarget)
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 100.0f);
+
+        if (hit.transform != null)
+        {
+            if (hit.collider.CompareTag("Device"))
+            {
+                Vector2 objectPosition = Camera.main.WorldToViewportPoint((Vector2)hit.transform.position);
+
+                Vector2 position = new Vector2((objectPosition.x - 0.5f) * 1600, (objectPosition.y - 0.5f) * 900);
+
+                line.SetPointPosition(1, position);
+
+                MeshLineManager.Instance.Connect(line);
+
+                int lv;
+
+                int.TryParse(hit.transform.gameObject.name, out lv);
+
+                GameManager.Instance.StageLv = lv;
+
+                SceneChanger.Instance.ChangeScene(SceneType.InGame);
+            }
+        }
+        else
+        {
+            MeshLineManager.Instance.Clear(line);
+        }
     }
 }
