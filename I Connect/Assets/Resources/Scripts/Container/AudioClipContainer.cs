0bbefabb8f53d8c4d3c0e10377958d5281e848c0
정지상 <ashuatz@gmail.com>
2017-08-18 01:39:44 +0900

    SoundContainer add

---
+++ I Connect/Assets/Resources/Scripts/Container/AudioClipContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioClipContainer : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] m_Clips;
+
+    public AudioClip this[int index]
+    {
+        get
+        {
+            return m_Clips[index];
+        }
+    }
+}
---
+++ I Connect/Assets/Resources/Scripts/Container/AudioSourceContainer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceContainer : MonoBehaviour 
+{
+    [SerializeField]
+    private AudioSource[] m_Sources;
+
+    public AudioSource this[int index]
+    {
+        get
+        {
+            return m_Sources[index];
+        }
+    }
+    public int Length { get { return m_Sources.Length; } }
+}
