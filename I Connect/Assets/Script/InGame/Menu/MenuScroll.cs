0279d22736c07308103def756b9dceed8f2ecfee
HyunDo_Lee <godd7776@gmail.com>
2017-08-17 19:09:47 +0900

    [ Create ] MenuScroll, Shapes

---
+++ I Connect/Assets/Script/InGame/Menu/MenuScroll.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuScroll : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    [SerializeField]
+    private Transform menuTrans;
+
+    private Vector2 
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+}
