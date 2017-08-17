50b18668d8fbf28633a0f8f0a419cd3f2490e1c7
정지상 <ashuatz@gmail.com>
2017-08-18 00:10:53 +0900

    [title Effect]

---
+++ I Connect/Assets/Script/CubeTextureSet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTextureSet : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector2[] UVs = new Vector2[mesh.vertices.Length];
+        // Front
+        UVs[0] = new Vector2(0.0f, 0.0f);
+        UVs[1] = new Vector2(0.333f, 0.0f);
+        UVs[2] = new Vector2(0.0f, 0.333f);
+        UVs[3] = new Vector2(0.333f, 0.333f);
+        // Top
+        UVs[9] = new Vector2(0.334f, 0.334f);  //16 17 18 19
+        UVs[8] = new Vector2(0.334f, 0.666f);
+        UVs[4] = new Vector2(0.666f, 0.666f);
+        UVs[5] = new Vector2(0.666f, 0.334f);
+        // Back
+        UVs[6] = new Vector2(1.0f, 0.0f);
+        UVs[7] = new Vector2(0.667f, 0.0f);
+        UVs[10] = new Vector2(1.0f, 0.333f);
+        UVs[11] = new Vector2(0.667f, 0.333f);
+        // Bottom
+        UVs[12] = new Vector2(0.0f, 0.334f);
+        UVs[13] = new Vector2(0.0f, 0.666f);
+        UVs[14] = new Vector2(0.333f, 0.666f);
+        UVs[15] = new Vector2(0.333f, 0.334f);
+        // Left
+        UVs[17] = new Vector2(0.334f, 0.333f);
+        UVs[18] = new Vector2(0.666f, 0.333f);
+        UVs[16] = new Vector2(0.334f, 0.0f);
+        UVs[19] = new Vector2(0.666f, 0.0f);
+        // Right        
+        UVs[20] = new Vector2(0.667f, 0.334f);
+        UVs[21] = new Vector2(0.667f, 0.666f);
+        UVs[22] = new Vector2(1.0f, 0.666f);
+        UVs[23] = new Vector2(1.0f, 0.334f);
+        mesh.uv = UVs;
+    }
+}
---
+++ I Connect/Assets/Script/Menu/TitleManager_2.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager_2 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Names;
+
+    [SerializeField]
+    private bool tester;
+    [SerializeField]
+    private Transform Marker;
+
+    private Quaternion defaultRotate;
+    private Quaternion ReverseRotate;
+
+    private void Awake()
+    {
+        for(int i=0;i<Names.Length;++i)
+        {
+            Names[i].SetActive(false);
+            Names[i].transform.position = Marker.position;
+        }
+        defaultRotate = Quaternion.Euler(-180, 0, 0);
+    }
+
+    private void Update()
+    {
+        if(tester)
+        {
+            tester = false;
+            StartCoroutine(StartTitle());
+        }
+    }
+    public IEnumerator StartTitle()
+    {
+        int indexer = 0;
+        Names[indexer].SetActive(true);
+        while (indexer < Names.Length-1)
+        {
+            yield return new WaitForSeconds(0.5f);
+            defaultRotate = Quaternion.Euler(Random.Range(1500, 2700), Random.Range(1500, 2700), Random.Range(1500, 2700));
+            StartCoroutine(Tween.TweenTransform.Rotation(Names[indexer].transform, defaultRotate, 0.25f));
+            yield return new WaitForSeconds(0.35f);
+            defaultRotate = Quaternion.Euler(Random.Range(1500, 2700), Random.Range(1500, 2700), Random.Range(1500, 2700));
+            StartCoroutine(Tween.TweenTransform.Rotation(Names[indexer].transform, defaultRotate, 0.25f));
+            yield return new WaitForSeconds(0.24f);
+            Names[indexer].SetActive(false);
+            if (indexer + 1 >= Names.Length)    break;
+            Names[++indexer].SetActive(true);
+        }
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(Tween.TweenTransform.Rotation(Names[8].transform, Quaternion.Euler(0,-135,0), 0.25f));
+        yield return new WaitForSeconds(0.5f);
+
+        yield return null;
+    }
+}
