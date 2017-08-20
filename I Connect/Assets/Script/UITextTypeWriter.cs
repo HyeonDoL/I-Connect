2e29928ef448d456f845d28627092aeabd010b55
정지상 <ashuatz@gmail.com>
2017-08-20 17:55:14 +0900

    [loading ENd] -need create more loading image

---
+++ I Connect/Assets/Script/UITextTypeWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextTypeWriter : MonoBehaviour {
+
+    [SerializeField]
+    private Text txt;
+    private string story;
+    [SerializeField]
+    private float WaitTime;
+    void Start()
+    {
+        story = txt.text;
+        txt.text = "";
+
+        // TODO: add optional delay when to start
+        StartCoroutine("PlayText");
+    }
+
+    IEnumerator PlayText()
+    {
+        foreach (char c in story)
+        {
+            txt.text += c;
+            yield return new WaitForSeconds(WaitTime);
+        }
+    }
+}
